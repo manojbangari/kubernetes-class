2229cec91c7914aa9da4c30925903421d7826329
Manoj <manojbangari39@gmail.com>
2021-01-12 07:30:01 -0800

    added files

--- script1.sh
+++ script1.sh
@@ -4,15 +4,15 @@
 
 echo "*************************"
 
-pod=`kubectl get pods  |  grep -iv "running" | awk '{print $1}'| tail -1`
+pod=`kubectl get pods  |  grep -iv "running" | awk '{print $1}'| tail -n +3`
 
 
 
 echo $pod
 
-kubectl get pods > out.txt
+#kubectl get pods > out.txt
 
-echo -e "Alert $pod is down\n\n\n .. $out " | mail -a out.txt  -s "Alert Production" manojbangari2721@gmail.com
+#echo -e "Alert $pod is down\n\n\n .. $out " | mail -a out.txt  -s "Alert Production" manojbangari2721@gmail.com
 
 
 
