87b9c30aa97b5de30f4b0a94a02b7906b3a218fe
Manoj <manojbangari39@gmail.com>
2021-01-14 10:40:01 -0800

    added files

--- script1.sh
+++ script1.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
-
-
 rm -rf describe.out
-
 echo "*************************"
-
 kubectl get pods  |  grep -iv "running" | awk '{print $1}'| tail -n +2 > podstatus.txt
-
-count=`cat podstatus.txt| wc -l `
-
+count=`cat podstatus.txt| wc -l`
 for i in  $(cat podstatus.txt); do kubectl describe pod $i   >> describe.out ;done 
-
 kubectl get pods > out.txt
-
-
-echo -e "Alert $pod is down\n\n\n ..  " | mail -a out.txt -a describe.out  -s "Alert Production" manojbangari39@gmail.com
-
+echo -e "Alert pods  is down\n\n\n ..  " | mail -a out.txt -a describe.out  -s "Alert Production" manojbangari39@gmail.com
 
 
 
